c3226218005e8b83d0021f819d28d48a4a30a0a0
Pierre-Alexandre Machard <pierrealexandre.machard@hotmail.fr>
2016-04-29 10:08:24 +0200

    Premier commit sur fichier test
    * Contenu du fichier vidé

---
+++ RadarGit/src/radarHF.py
@@ -0,0 +1,14 @@
+# -*-coding:utf-8 -*
+# -*-coding:Latin-1 -*
+
+# title           : RadarHF.py
+# description     : Lit et traite les donnees d'un fichier radar HF
+# author          : Pierre-Alexandre Machard
+# date            : 29042016
+# version         : 1.0
+# version_python  : 2.7
+
+# ==============================================================================
+
+if __name__ == "__main__":
+    print "Jeanne ! Au secours !"
